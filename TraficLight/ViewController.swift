3cfaafbf465171bf57db456a8defe33b0ae2f69f
VK <art.nebar@mail.ru>
2022-11-07 23:42:27 +0300

    Past Array

--- TraficLight/ViewController.swift
+++ TraficLight/ViewController.swift
@@ -8,12 +8,33 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    private var changeColor = 0
+    
+    @IBOutlet var changeButton: UIButton!
+    @IBOutlet var trafickLightColor:[UIView]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        for color in trafickLightColor {
+            color.layer.cornerRadius = 50
+            color.alpha = 0.3
+        }
+
+        changeButton.layer.cornerRadius = 10
     }
-
-
+    
+    @IBAction func changeButtonDidTapped(_ sender: Any) {
+        
+        if changeColor == trafickLightColor.count { changeColor = 0}
+        
+        for color in trafickLightColor {
+            color.alpha = 0.3
+        }
+        trafickLightColor[changeColor].alpha = 1
+        changeColor += 1
+        changeButton.setTitle( "NEXT", for: .normal)
+    }
 }
 
